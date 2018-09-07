36887719c7d638d6ba3fd20d6a93d228faf8c46a
wang_zaiming <wang_zaiming@aliyun.com>
2018-09-07 20:19:23 +0800

    第一次上传login

---
+++ login/application/gzck/config.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wenfei
+ * Date: 2017/2/13
+ * Time: 14:20
+ */
+return [
+    'view_replace_str' => [
+        '__PUBLIC__' => 'http://localhost/login/public/static'
+    ]
+];
---
+++ login/application/gzck/database.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wenfei
+ * Date: 2017/2/15
+ * Time: 14:26
+ */
+
+return [
+    'database' => 'gzrbimm',
+];
