1c301f96c241708aafac9f998dfd39b94d4e9d0d
wang_zaiming <wang_zaiming@aliyun.com>
2018-09-30 21:50:57 +0800

    remove ueditor and add kindEditor

---
+++ login/application/admin/controller/Icon.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wang
+ * Date: 18-9-29
+ * Time: 下午4:51
+ */
+// Make sure you are using a correct path here.
+namespace app\admin\controller;
+use think\Controller;
+
+class Icon extends Controller
+{
+    public function index(){
+
+    }
+
+}
+
